98d1d9c1f9a34500c4a5aeaeb2d1735773ba652b
Yogeshk108 <56926977+Yogeshk108@users.noreply.github.com>
2025-05-19 01:36:51 +0530

    Added Mock Database

---
+++ app/database/mock_database.py
@@ -0,0 +1,75 @@
+DATA = [
+    {
+        "id": 1,
+        "data": "Developed and deployed a cost calculation system processing 120,000 daily API "
+                "calls, integrating AWS Lambda, Athena, Kafka, and PostgreSQL for data ingestion "
+                "and reporting via a QuickSight dashboard."
+    },
+    {
+        "id": 2,
+        "data": "Enhanced security in CI/CD by implementing OpenID Connect OIDC "
+                "authentication for AWS CLI in CircleCI pipelines"
+    },
+    {
+        "id": 3,
+        "data": "Spearheaded the development and deployment of a complex API integration for a "
+                "pilot launch that handled thousands of daily prediction requests from 5,000 users"
+    },
+    {
+        "id": 4,
+        "data": "Managed a seamless migration of the Elastic Kubernetes Service EKS cluster from "
+                "version 1.21 to 1.29, optimizing and streamlining operations to achieve "
+                "a 20 percent reduction in AWS costs"
+    },
+    {
+        "id": 5,
+        "data": "Accelerated model production readiness by 10 percent by developing a Kafka "
+                "utility package, simplifying Kafka integration for developers, "
+                "and improving efficiency"
+    },
+    {
+        "id": 6,
+        "data": "Optimized system architecture by segregating Kafka topics by model type, "
+                "resulting in eliminating bottlenecks and a 50 percent performance improvement"
+    },
+    {
+        "id": 7,
+        "data": "Architected and implemented a notification service that monitors and "
+                "communicates status updates for data pipelines, tests, and batch processes"
+    },
+    {
+        "id": 8,
+        "data": "Optimized API response speed by 50 percent by implementing compressed "
+                "streaming responses and Python generators, enhancing memory management"
+    },
+    {
+        "id": 9,
+        "data": "Developed a high-throughput batch framework using FastAPI and Kubernetes Cron, "
+                "processing 120,000 daily requests for internal teams"
+    },
+    {
+        "id": 10,
+        "data": "Engineered user-defined task execution order identification, reducing rule model "
+                "execution time by using graph data structures, specifically "
+                "a Directed Acyclic Graph with topological sort"
+    },
+    {
+        "id": 11,
+        "data": "Ensured code reliability and quality with 100 percent unit test coverage by "
+                "writing comprehensive unit tests"
+    },
+    {
+        "id": 12,
+        "data": "Coded API wrapper for a data science model using Fast API, "
+                "including standardizing request and response structure"
+    },
+    {
+        "id": 13,
+        "data": "Created technical documentation and API contracts for the developed service"
+    },
+    {
+        "id": 14,
+        "data": "Mentored and provided technical guidance to junior developers, "
+                "conducting regular code reviews"
+    }
+]
