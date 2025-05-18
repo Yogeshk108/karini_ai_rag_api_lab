2942dc9a8a62c0ed2ca78e5790732df624ce809b
Yogeshk108 <56926977+Yogeshk108@users.noreply.github.com>
2025-05-19 01:40:34 +0530

    Added Pydantic Schema for Input

---
+++ app/schema_model/schema.py
@@ -0,0 +1,11 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class RequestModel(BaseModel):
+    """
+    A pydantic model to validate input request body
+    """
+    query: str = Field(..., min_length=1)
+    top_k: Optional[int] = Field(2, ge=0)
